82a9a189f9d4849627f72f09532cf81d335c6224
Cooper7196 <cbrosgall@gmail.com>
2024-05-19 20:40:41 -0700

    More sim work

--- main realistic.py
+++ main realistic.py
@@ -1,6 +1,75 @@
 import random
 import pygame
 import math
+import math
+import numpy as np
+from scipy.signal import StateSpace
+import matplotlib.pyplot as plt
+
+dt = 0.01
+
+# Robot Parameters
+mass = 15 / 2.205
+mu = 0.1
+inertia = 0.1
+track_radius = 6 * 0.0254
+gear_ratio = 1/1.5
+wheel_radius = 2 * 0.0254
+num_motors_per_side = 3
+
+# Motor Parameters
+nominal_voltage = 12.0
+stall_torque = 2.1286320584285905 * num_motors_per_side
+stall_current = 2.5029144189407573 * num_motors_per_side
+free_current = 0.13 * num_motors_per_side
+free_speed = 200
+
+# convert rpm to rad/s
+free_speed = free_speed / 60 * (2 * np.pi)
+
+
+resistance = nominal_voltage / stall_current
+torque_const = stall_torque / stall_current
+angular_vel_const = free_speed / (nominal_voltage - resistance * free_current)
+
+# Inputs
+input_voltage_left = 10
+input_voltage_right = 12
+
+
+C1 = -(gear_ratio ** 2 * torque_const) / \
+    (angular_vel_const * resistance * wheel_radius**2)
+C2 = (gear_ratio * torque_const) / (resistance * wheel_radius)
+
+A = np.array(
+    [
+        [0, -1/(2 * track_radius), 1/(2 * track_radius)],
+        [0, (1/mass + track_radius**2 / inertia) * C1,
+         (1/mass - track_radius**2 / inertia) * C1],
+        [0, (1/mass - track_radius**2 / inertia) * C1,
+         (1/mass + track_radius**2 / inertia) * C1]
+    ]
+)
+B = np.array(
+    [
+        [0, 0],
+        [(1/mass + track_radius**2 / inertia) * C2,
+         (1/mass - track_radius**2 / inertia) * C2],
+        [(1/mass - track_radius**2 / inertia) * C2,
+         (1/mass + track_radius**2 / inertia) * C2]
+    ]
+)
+
+C = np.eye(3)
+D = np.zeros((3, 2))
+
+sys = StateSpace(A, B, C, D)
+
+sim = sys.to_discrete(dt)
+
+
+
+
 
 def inches_to_pixels(inches):
     return inches * (4881.0 / 140.41) / 5
@@ -10,15 +79,14 @@ def pixels_to_inches(pixels):
 
 dt = 0.01
 class Pose:
-    x = 0
-    y = 0
     leftSpeed = 0
     rightSpeed = 0
     angularSpeed = 0
     theta = 0
+    state = np.zeros((3, 1))
     def __init__(self, x, y, theta):
-        self.x = x
-        self.y = y
+        self.x_pos = x
+        self.y_pos = y
         self.xSpeed = 0
         self.ySpeed = 0
         self.theta = theta
@@ -31,87 +99,66 @@ class Pose:
         max_accel = inches_to_pixels(1500000)
 
 
-
         leftWheelTarget = min(max(left, -max_speed), max_speed)
         rightWheelTarget = min(max(right, -max_speed), max_speed)
         
-        leftWheelTarget = inches_to_pixels(left)
-        rightWheelTarget = inches_to_pixels(right)
-        if (leftWheelTarget - self.lastLeftWheelSpeed) / dt > max_accel:
-            print(f"Left Limited at {(leftWheelTarget - self.lastLeftWheelSpeed) / dt} m/s^2")
-            leftWheelSpeed = self.lastLeftWheelSpeed + max_accel * dt
-        elif (leftWheelTarget - self.lastLeftWheelSpeed) / dt < -max_accel:
-            print(f"Left Limited at {(leftWheelTarget - self.lastLeftWheelSpeed) / dt} m/s^2")
-            leftWheelSpeed = self.lastLeftWheelSpeed - max_accel * dt
+        self.state = sim.A @ self.state + \
+            sim.B @ np.array([[left], [right]])
+
+        leftSpeed = self.state[1][0]
+        rightSpeed = self.state[2][0]
+
+
+        if leftSpeed == rightSpeed:
+            xSpeed = leftSpeed * math.cos(self.theta) * dt
+            ySpeed = leftSpeed * math.sin(self.theta) * dt
+            angularSpeed = 0
         else:
-            leftWheelSpeed = leftWheelTarget
+            turn_radius = track_radius * \
+                (leftSpeed + rightSpeed) / (rightSpeed - leftSpeed)
+            ICC_x = self.x_pos - inches_to_pixels(turn_radius/0.0254) * math.sin(self.theta)
+            ICC_y = self.y_pos + \
+                inches_to_pixels(turn_radius/0.0254) * math.cos(self.theta)
 
-        if (rightWheelTarget - self.lastRightWheelSpeed) / dt > max_accel:
-            print(f"Right Limited at a{pixels_to_inches((rightWheelTarget - self.lastRightWheelSpeed) / dt)} m/s^2")
-            rightWheelSpeed = self.lastRightWheelSpeed + max_accel * dt
-        elif (rightWheelTarget - self.lastRightWheelSpeed) / dt < -max_accel:
-            print(f"Right Limited at b{(rightWheelTarget - self.lastRightWheelSpeed) / dt} m/s^2")
-            rightWheelSpeed = self.lastRightWheelSpeed - max_accel * dt
-        else:
-            rightWheelSpeed = rightWheelTarget
-            
-        self.lastLeftWheelSpeed = leftWheelSpeed
-        self.lastRightWheelSpeed = rightWheelSpeed
-        # skid steer drive
-        # left and right are between -1 and 1
+            # draw line to ICC
+            # print((int(
+            #     ICC_x), int(ICC_y)))
+            pygame.draw.circle(screen, (0, 255, 0), (int(
+                ICC_x), int(ICC_y)), abs(int(turn_radius)), 1)
 
-        track_width = inches_to_pixels(11)
-
-
-        
-        centrifugalAccel = 0
-        if leftWheelSpeed == rightWheelSpeed:
-            self.xSpeed = leftWheelSpeed * math.cos(self.theta) * dt
-            self.ySpeed = leftWheelSpeed * math.sin(self.theta)  * dt
-            self.angularSpeed = 0
-        else:
-            ICC_R = (track_width / 2) * ((leftWheelSpeed + rightWheelSpeed) / (rightWheelSpeed - leftWheelSpeed))
-            omega = (rightWheelSpeed - leftWheelSpeed) / track_width
-
+            vel = (leftSpeed + rightSpeed) / 2
+            omega = (rightSpeed - leftSpeed) / (track_radius * 2)
 
             # print(pixels_to_inches(ICC_R))
+            # centipetal_force = mass * vel ** 2 / abs(turn_radius)
+            # centipetal_force = 0
+            # friction_force = mu * 9.81 * mass
+
+            # if centipetal_force < friction_force:
+            #     centipetal_force = friction_force
             
-            ICC_x = self.x - ICC_R * math.sin(self.theta)
-            ICC_y = self.y + ICC_R * math.cos(self.theta)
+            # net_force = centipetal_force - friction_force
 
-            xSpeed = (math.cos(omega * dt) * (self.x - ICC_x) - math.sin(omega * dt) * (self.y - ICC_y) + ICC_x) - self.x
-            ySpeed = (math.sin(omega * dt) * (self.x - ICC_x) + math.cos(omega * dt) * (self.y - ICC_y) + ICC_y) - self.y
+            # net_accel = net_force / mass
+
+            # net_accel = math.copysign(net_accel, turn_radius)
+
+            # Apply Acceleration
+        
+            xSpeed = vel * math.cos(self.theta) * dt
+            ySpeed = vel * math.sin(self.theta) * dt
+
+            angularSpeed = omega * dt
             
-            # print(self.xSpeed - xSpeed, self.ySpeed - ySpeed)
-
-            # Add vector components
-
-            # # Get vector form
-            # nextSpeedAngle = math.atan2(nextYSpeed, nextXSpeed)
-            # nextSpeedMagnitude = math.sqrt(nextXSpeed * nextXSpeed + nextYSpeed * nextYSpeed)
-
-            # # Get magnitude along robot angle
-            # nextSpeedMagnitude = nextSpeedMagnitude * math.cos(nextSpeedAngle - self.theta)
-
-            # # Convert back to components
-            # nextXSpeed = nextSpeedMagnitude * math.cos(nextSpeedAngle)
-            # nextYSpeed = nextSpeedMagnitude * math.sin(nextSpeedAngle)
-
-
-            self.xSpeed = xSpeed
-            self.ySpeed = ySpeed
-            self.angularSpeed = omega * dt
             # centrifugalAccel = 2 * self.angularSpeed * ICC_R
-        # print(pixels_to_inches(self.xSpeed), pixels_to_inches(self.ySpeed))
+        # print(pixels_to_inches(x_posSpeed), pixels_to_inches(self.ySpeed))
 
         # Apply centrifugal acceleration
-        # self.x -= math.sin(self.theta) * centrifugalAccel * dt
+        # x_pos -= math.sin(self.theta) * centrifugalAccel * dt
         # self.y += math.cos(self.theta ) * centrifugalAccel * dt
-
-        self.x += self.xSpeed
-        self.y += self.ySpeed
-        # print(centrifugalAccel)
-        self.theta += self.angularSpeed
+        self.x_pos += inches_to_pixels(xSpeed / 0.0254)
+        self.y_pos += inches_to_pixels(ySpeed / 0.0254)
+        self.theta += angularSpeed
     def draw(self):
         robot_size = inches_to_pixels(15)
         image = pygame.Surface((robot_size, robot_size), pygame.SRCALPHA)
@@ -122,13 +169,13 @@ class Pose:
 
         rect = image.get_rect()
 
-        rect.center = (self.x, self.y)
+        rect.center = (self.x_pos, self.y_pos)
         image, rect = rotate(orig_image, rect, -self.theta * 180 / math.pi)
         screen.blit(image, rect)
     def __repr__(this):
-        return "Pose(" + str(this.x) + ", " + str(this.y) + ", " + str(this.theta) + ")"
+        return "Pose(" + str(this.x_pos) + ", " + str(this.y_pos) + ", " + str(this.theta) + ")"
     def get_pose(this):
-        return (pixels_to_inches(this.x), pixels_to_inches(this.y), this.theta)
+        return (pixels_to_inches(this.x_pos), pixels_to_inches(this.y_pos), this.theta)
     
 def rotate(image, rect, angle):
     # Rotate the original image without modifying it.
@@ -160,9 +207,9 @@ counter = 0
 
 # joystick = []
 
-# for i in range(0, pygame.joystick.get_count()):
-#     joystick = pygame.joystick.Joystick(i)
-# joystick.init()
+for i in range(0, pygame.joystick.get_count()):
+    joystick = pygame.joystick.Joystick(i)
+joystick.init()
 
 
 def wheel_speeds(velocity, angular_velocity):
@@ -234,26 +281,27 @@ while True:
     keys=pygame.key.get_pressed()
     screen.blit(bg, (0, 0))
 
-    # left_input = -joystick.get_axis(1) * 60
-    # right_input = -joystick.get_axis(3)  * 60
-    left_input, right_input = wheel_speeds(*get_cur_vel(curDist))
+    left_input = -joystick.get_axis(1) * 12
+    right_input = -joystick.get_axis(3)  * 12
+    # left_input, right_input = wheel_speeds(*get_cur_vel(curDist))
     if i == -1:
         left_input = 0
         right_input = 0
+    # print(f"Left In :{right_input:.2f}, right in: {left_input:.2f}")
     robot.move(left_input, right_input, realDt)
-    positions.append((robot.x, robot.y))
+    positions.append((robot.x_pos, robot.y_pos))
     for pos in positions:
         pygame.draw.circle(screen, (255, 0, 0), (int(pos[0]), int(pos[1])), 2)
-    for pos in pathPoints:
-        pygame.draw.circle(screen, (0, 255, 0), (inches_to_pixels(pos[0]) + inches_to_pixels(84), inches_to_pixels(pos[1]) + inches_to_pixels(108)), 2)
+    # for pos in pathPoints:
+    #     pygame.draw.circle(screen, (0, 255, 0), (inches_to_pixels(pos[0]) + inches_to_pixels(84), inches_to_pixels(pos[1]) + inches_to_pixels(108)), 2)
     robot.draw()
     
-    if i < len(speeds) - 1 and i != -1:
-        i += 1
-        print(f"Left In :{right_input:.2f}, right in: {left_input:.2f}")
-    else:
-        i = -1
-        print(f"X: {pixels_to_inches(robot.x):.2f}, Y: {pixels_to_inches(robot.y):.2f}, Theta: {robot.theta * (180 / math.pi):.2f}")
+    # if i < len(speeds) - 1 and i != -1:
+    #     i += 1
+    #     print(f"Left In :{right_input:.2f}, right in: {left_input:.2f}")
+    # else:
+    #     i = -1
+    #     print(f"X: {pixels_to_inches(robot.x):.2f}, Y: {pixels_to_inches(robot.y):.2f}, Theta: {robot.theta * (180 / math.pi):.2f}")
     curPose = robot.get_pose()
     curDist += math.sqrt((lastPos[0] - curPose[0]) ** 2 + (lastPos[1] - curPose[1]) ** 2)
     lastPos = curPose
